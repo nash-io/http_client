855e45d4820ee21dc2442f564aef14c4540cc6d3
Rafael Scheffer <rafaelscheff@gmail.com>
2025-05-07 17:14:40 +0200

    Add patch function

--- lib/http_client.ex
+++ lib/http_client.ex
@@ -15,32 +15,32 @@ defmodule HttpClient do
   @callback put(String.t(), String.t(), list()) :: {:ok, any()} | {:error, any()}
   @callback put(String.t(), String.t(), list(), list()) :: {:ok, any()} | {:error, any()}
 
+  @callback patch(String.t(), String.t()) :: {:ok, any()} | {:error, any()}
+  @callback patch(String.t(), String.t(), list()) :: {:ok, any()} | {:error, any()}
+  @callback patch(String.t(), String.t(), list(), list()) :: {:ok, any()} | {:error, any()}
+
   @callback delete(String.t()) :: {:ok, any()} | {:error, any()}
   @callback delete(String.t(), list()) :: {:ok, any()} | {:error, any()}
   @callback delete(String.t(), list(), Keyword.t()) :: {:ok, any()} | {:error, any()}
 
   def get(url), do: request(:get, [url])
-
   def get(url, headers), do: request(:get, [url, headers])
-
   def get(url, headers, options), do: request(:get, [url, headers, options])
 
   def post(url, body), do: request(:post, [url, body])
-
   def post(url, body, headers), do: request(:post, [url, body, headers])
-
   def post(url, body, headers, options), do: request(:post, [url, body, headers, options])
 
   def put(url, body), do: request(:put, [url, body])
-
   def put(url, body, headers), do: request(:put, [url, body, headers])
-
   def put(url, body, headers, options), do: request(:put, [url, body, headers, options])
 
+  def patch(url, body), do: request(:patch, [url, body])
+  def patch(url, body, headers), do: request(:patch, [url, body, headers])
+  def patch(url, body, headers, options), do: request(:patch, [url, body, headers, options])
+
   def delete(url), do: request(:delete, [url])
-
   def delete(url, headers), do: request(:delete, [url, headers])
-
   def delete(url, headers, options), do: request(:delete, [url, headers, options])
 
   defp request(method, [url | _] = args) do
