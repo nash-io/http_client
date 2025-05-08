1ae6781b188b30136add7ae6ae29018e41c22aa3
Rafael Scheffer <rafaelscheff@gmail.com>
2025-05-08 20:16:45 +0200

    Bump version to 0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HttpClient.MixProject do
   def project do
     [
       app: :http_client,
-      version: "0.2.5",
+      version: "0.3.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
