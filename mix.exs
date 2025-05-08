d97e432a80a3e6492b38e4d47ea6bea06a017c08
Rafael Scheffer <rafaelscheff@gmail.com>
2025-05-08 20:25:12 +0200

    Add organization

--- mix.exs
+++ mix.exs
@@ -48,6 +48,7 @@ defmodule HttpClient.MixProject do
     [
       # This option is only needed when you don't want to use the OTP application name
       name: "http_client_nash",
+      organization: "neonexchange",
       # These are the default files included in the package
       files: ~w(lib .formatter.exs mix.exs README*),
       licenses: ["Apache-2.0"],
